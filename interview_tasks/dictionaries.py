6b69220daaabccef9ca488ec980b0f9a9f845e0a
DRoskowska <d.roskowska@striped-giraffe.com>
2020-05-13 13:18:24 +0200

    Initial commit

---
+++ interview_tasks/dictionaries.py
@@ -0,0 +1,21 @@
+"""
+Implement a group_by_owners function that:
+- Accepts a dictionary containing the file owner name for each file name.
+- Returns a dictionary containing a list of file names for each owner name, in any order.
+
+For example, for a dictionary:
+    {'Input.txt': 'Romek', 'Code.py': 'Staszek', 'Output.txt': 'Romek'} 
+the group_by_owners function should return: 
+    {'Romek': ['Input.txt', 'Output.txt'], 'Staszek': ['Code.py']}.
+"""
+group_by_owners()
+//
+owners = []
+for x in files:
+    owners.add(files[x])
+
+for key in a_dict:
+    ...
+print(key, '->', a_dict[key])
+
+return owners
---
+++ interview_tasks/palindrome.py
@@ -0,0 +1,7 @@
+'''
+Write an algorithm which will check if the given word is a palindrome
+(expression which sounds the same whether it's read from left to right or right to left).
+'''
+for i in palindrome:
+  max = palindrome.length
+  if palindrome[0+i] == palindrome[max-i]
